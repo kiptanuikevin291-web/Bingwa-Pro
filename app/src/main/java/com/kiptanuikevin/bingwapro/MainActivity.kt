8a6295844028ca3a199b591512462d54f6725f6b
Rutto998 <kiptanuikevin204@gmail.com>
2026-07-15 14:03:54 +0300

    Connect dashboard to database

--- app/src/main/java/com/kiptanuikevin/bingwapro/MainActivity.kt
+++ app/src/main/java/com/kiptanuikevin/bingwapro/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.kiptanuikevin.bingwapro
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = DatabaseHelper(this)
+        val stats = db.getStats()
+
+        findViewById<TextView>(R.id.smsCount).text = stats.first.toString()
+        findViewById<TextView>(R.id.tokenCount).text = stats.second.toString()
+        findViewById<TextView>(R.id.subscriberCount).text = stats.third.toString()
     }
 }
